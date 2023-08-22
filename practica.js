7fbbc5a52c97bf686590bb865ad72e060287364d
axelcasco <axelguillermocasco@gmail.com>
2023-08-21 21:07:42 -0300

    first commit

---
+++ practica.js
@@ -0,0 +1,24 @@
+
+/* var nombre = prompt ("hola como te llamas? ")
+alert ("hola " + nombre )
+
+var res = prompt ("ingrese un numero")
+var res2= prompt ("ingrese otro numero")
+var num = Number (res)
+var num2 = Number (res2)
+*/
+/* var res1 = Number (prompt ("ingrese un numero"))
+var res2 = Number (prompt ("ingrese otro numero"))
+
+var res3 = res1 + res2 
+alert ("la suma de los numeros es " + res3) */
+
+// pedirle al usuario que ingrese su nombre // 
+// pedirle al usuario que ingrerse su edad //
+// mostrar un mensaje que diga hola (nombre), tu edad es (edad), Naciste en el año (año de nacimiento)//
+
+var nombre = prompt ("ingresa tu nombre")
+var edad= Number (prompt ("ingresa tu edad")) 
+var año= 2023 - edad 
+
+alert ("Tu nombre es " + nombre + " tenes " + edad + " años de edad y naciste en el año " + año )
---
+++ script.js
@@ -0,0 +1,27 @@
+
+//console.log lo usaos para escribir mensajes en la consola del navegador
+console.log ("Hola mundo desde la consola")
+
+//tipos de datos
+console.log (10 + 10)
+console.log (10 - 10 )
+
+//sumar strings
+
+console.log ("hola " + "mundo")
+console.log ("20" + "hola")
+
+//variable 
+var saludo = "hola"
+var num= 10
+var resultado = saludo + num
+
+console.log (saludo + " mundo")
+console.log (num + 20)
+console.log (resultado) 
+//alert
+var mensaje = ("hola mundo")
+alert (mensaje)
+
+//promt
+prompt ("hola como te llamas?")
